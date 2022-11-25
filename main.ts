2d3f84fae0765bfdc3dff2d23dfbf46cad5b6d80
sololim <101944276+sololim@users.noreply.github.com>
2022-11-24 20:51:36 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,8 @@
+basic.forever(function () {
+    if (true) {
+        basic.showArrow(ArrowNames.North)
+        basic.showArrow(ArrowNames.East)
+        basic.showArrow(ArrowNames.South)
+        basic.showArrow(ArrowNames.West)
+    }
+})
